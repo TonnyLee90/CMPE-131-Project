8186713e13550ed5f1fbec6562575e91b4bd0fb7
Tonny Lee <tonny.lee@sjsu.edu>
2025-04-25 14:47:11 -0700

    add layout/ login.html, login form, and add login route

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, EmailField, validators
 
+
+class LoginForm(FlaskForm):
+    email = EmailField('Username', validators=[validators.InputRequired()])
+    password = PasswordField('Password', validators=[validators.InputRequired()])
+    submit =  SubmitField("Login")
