d642616e205ba92266cff91c6ade79776413f1b6
Tonny Lee <tonny.lee@sjsu.edu>
2025-04-24 19:34:51 -0700

    add models and db

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+myapp = Flask(__name__)
+# set up db location
+myapp.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
+db = SQLAlchemy(myapp)
+from app import routes
